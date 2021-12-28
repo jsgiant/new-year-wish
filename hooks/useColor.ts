78e417570f74f97a5a63b6f2d39998cb86fbd42f
Anil Peddireddy <peddireddyanilchowdary@gmail.com>
2021-12-28 08:33:23 +0530

    Adds color utils and on change handlers

--- hooks/useColor.ts
+++ hooks/useColor.ts
@@ -1,16 +1,21 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 import { colors } from "../constants/colors";
+import { getSelectedColor } from "../utils/ColorUtils";
 
 //colors list
 
 const useColor = () => {
-	const [currentColor, setCurrentColor] = useState(colors[0]);
+  const router = useRouter();
+  const { slug } = router.query;
+  const initialColor = slug ? getSelectedColor(parseInt(slug[1])) : colors[0];
+  const [currentColor, setCurrentColor] = useState(initialColor);
 
-	const setColor = (id: number) => {
-		const currentColor = colors.find((item) => id == item.id);
-		setCurrentColor(currentColor ?? colors[0]);
-	};
-	return { colors, setColor, currentColor };
+  const setColor = (id: number) => {
+    setCurrentColor(getSelectedColor(id));
+  };
+
+  return { colors, setColor, currentColor };
 };
 
 export default useColor;
