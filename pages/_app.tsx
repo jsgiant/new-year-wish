fbc83dc21aef9f7070fbf5c789830f15b4437aad
Anil Peddireddy <peddireddyanilchowdary@gmail.com>
2021-12-29 11:57:37 +0530

    Adds theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import useColor from "../hooks/useColor";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const { currentColor } = useColor();
+	return (
+		<ThemeProvider theme={currentColor}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
